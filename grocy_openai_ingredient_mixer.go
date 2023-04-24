9ee9462c3cca1d7ace1f631c85960fb64eaea044
fathermahoney <admin@secure-svr.net>
2023-04-24 15:45:26 -0500

    Updating to using the buffer

--- grocy_openai_ingredient_mixer.go
+++ grocy_openai_ingredient_mixer.go
@@ -17,6 +17,8 @@ import (
 	"time"
 	"log"
 	"strconv"
+	"bytes"
+	"bufio"
 )
 
 type Month int
@@ -41,7 +43,7 @@ var month = time.Now().Month()
 var day = time.Now().Day()
 var weekday = time.Now().Weekday()
 
-type Theme struct {
+type Personality struct {
 		Name   string `json:"name"`
 		Prompt struct {
 			Season   []string `json:"season"`
@@ -54,88 +56,18 @@ func randInt(min int, max int) int {
 	return min + rand.Intn(max-min)
 }
 
-func sendSlackmessage() {
-	RESPONSE_FILE := os.Getenv("RESPONSE_FILE")
-	content, err := ioutil.ReadFile(RESPONSE_FILE)
-	if err != nil {
-		log.Fatal("Err")
-	}
-
-	var slackMessage = string(content)
-	formattedSlackMessage := fmt.Sprintf(strings.Replace(slackMessage, "\"", "", 3))
-	SLACK_API_KEY := os.Getenv("SLACK_API_KEY")
-	api := slack.New(SLACK_API_KEY)
-	CHANNEL_ID := os.Getenv("CHANNEL_ID")
-	channelID, timestamp, err := api.PostMessage(
-		CHANNEL_ID,
-		slack.MsgOptionText(formattedSlackMessage, false),
-		slack.MsgOptionAsUser(false), // Add this if you want that the bot would post message as a user, otherwise it will send response using the default slackbot
-	)
-	if err != nil {
-		log.Printf("%s\n", err)
-		return
-	}
-	log.Printf("Ingredients mixed and Slack message successfully sent to channel %s at %s", channelID, timestamp)
-}
-
-func openai_ingredient_mixer() {
-	RESPONSE_FILE := os.Getenv("RESPONSE_FILE")
-	f, err := os.Create(RESPONSE_FILE) // create &/or clear the local file
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close() // close the file with defer
-
-	headline := fmt.Sprintf("*Tanglesprings Specials for %s %s %d, %d*\n", weekday, month, day, year)
-	f.Write([]byte(headline))
-	INVENTORY_FILE := os.Getenv("INVENTORY_FILE")
-	content, err := ioutil.ReadFile(INVENTORY_FILE) // opens the inventory file just created
-	if err != nil {
-		log.Fatal("Err")
-	}
-	var openaiPrompt = string(content)
-	OPENAI_API_KEY := os.Getenv("OPENAI_API_KEY")
-	c := openai.NewClient(OPENAI_API_KEY)
-	ctx := context.Background()
-	req := openai.ChatCompletionRequest{
-		Model:     openai.GPT3Dot5Turbo,
-		MaxTokens: 500,
-		Messages: []openai.ChatCompletionMessage{
-			{
-				Role:    openai.ChatMessageRoleUser,
-				Content: openaiPrompt,
-			},
-		},
-		Stream: true,
-	}
-	stream, err := c.CreateChatCompletionStream(ctx, req)
-	if err != nil {
-		log.Printf("ChatCompletionStream error: %v\n", err)
-		return
-	}
-	defer stream.Close()
-
-	for {
-		response, err := stream.Recv()
-		if errors.Is(err, io.EOF) {
-			return
-		}
-
-		if err != nil {
-			log.Printf("\nStream error: %v\n", err)
-			return
-		}
-		var responseContent = response.Choices[0].Delta.Content
-		f.Write([]byte(responseContent)) //write directly into file
-	}
-}
-
 func main() {
 	// Load .env file
 	err := godotenv.Load()
 	  if err != nil {
 		log.Fatal("Error loading .env file")
 	  }
+	  
+	// Create necessary buffers
+	var openai_buf bytes.Buffer
+	openai_writer := bufio.NewWriter(&openai_buf)
+	// openai_buf := bytes.Buffer{}
+	
 	// Create request to get all products from Grocy
 	GROCY_URL := os.Getenv("GROCY_URL")
 	url := GROCY_URL
@@ -160,65 +92,132 @@ func main() {
 		log.Fatal(err)
 		return
 	}
+	
 	var products []map[string]interface{} //defining the array of products
 	err = json.Unmarshal([]byte(body), &products)
+	
 	if err != nil {
 		log.Fatal("Error while decoding the data", err.Error())
 	}
-	INVENTORY_FILE := os.Getenv("INVENTORY_FILE")
-	f, err := os.Create(INVENTORY_FILE) // create/clear the inventory file
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close() // close the file with defer
 
+	// Load personality prompt file
 	PERSONALITIES := os.Getenv("PERSONALITIES")
 	jsonContent, err := ioutil.ReadFile(PERSONALITIES) // opens the inventory file just created
+	
 	if err != nil {
 		log.Fatal("Err")
 	}
+	
 	var jsonInput = string(jsonContent)
-	var Themes []Theme
-	err = json.Unmarshal([]byte(jsonInput), &Themes)
+	var Personalities []Personality
+	
+	err = json.Unmarshal([]byte(jsonInput), &Personalities)
 
 	if err != nil {
 		log.Fatal("JSON decode error!")
 		return
 	}
 
-	var names []string
-	for _, theme := range Themes {
-		names = append(names, theme.Name)
+	// get the length of the personalities object
+	var personality_names []string
+	for _, Personality := range Personalities {
+		personality_names = append(personality_names, Personality.Name)
 	}
 	
 	rand.Seed(time.Now().UTC().UnixNano())
-	var randomInt int = randInt(0, len(Themes))
-	var random_theme string = names[randomInt]
-	var theme_choice string
+	var randomInt int = randInt(0, len(Personalities))
+	var random_theme string = personality_names[randomInt]
+	var personality_choice string
 	
 	RANDOM_PROMPT, err := strconv.ParseBool(os.Getenv("RANDOM_PROMPT"))
 	if RANDOM_PROMPT {
-		theme_choice = random_theme
+		personality_choice = random_theme
 	} else {
-		theme_choice = os.Getenv("STATIC_PROMPT")
+		personality_choice = os.Getenv("STATIC_PROMPT")
 	}
 
-	for _, theme := range Themes {
-		if strings.TrimRight(theme_choice, "\n") == theme.Name {
-			f.Write([]byte(theme.Prompt.OpenAI)) //write directly into file
+	for _, Personality := range Personalities {
+		if strings.TrimRight(personality_choice, "\n") == Personality.Name {
+			openai_writer.WriteString(string([]byte(Personality.Prompt.OpenAI)))
+			openai_writer.Flush()
 		}
 	}
-
-	f.Write([]byte(os.Getenv("INGREDIENT_LIST_HANDLING"))) //write directly into file
+	openai_writer.WriteString(os.Getenv("INGREDIENT_LIST_HANDLING"))
+	openai_writer.Flush()
 	
 	for _, product := range products {
 		var active = int64(product["active"].(float64)) // in-stock units have positive active numbers
 		if active > 0 { // it's in stock, adding to the list
 			var name = product["name"].(string)
-			f.Write([]byte(name)) //write directly into file
-			f.WriteString(", ")
+			openai_writer.WriteString(name+",")
+			openai_writer.Flush()
 		}
 	}
-	openai_ingredient_mixer()
-	sendSlackmessage()
+	openai_ingredient_mixer(&openai_buf)
+}
+
+func openai_ingredient_mixer(openai_buf *bytes.Buffer) {
+	var slack_buf bytes.Buffer
+	writer := bufio.NewWriter(&slack_buf)
+	headline := fmt.Sprintf("*Tanglesprings Specials for %s %s %d, %d*\n", weekday, month, day, year)
+	writer.WriteString(headline)
+	writer.Flush()
+		
+	var openaiPrompt string = openai_buf.String()
+	OPENAI_API_KEY := os.Getenv("OPENAI_API_KEY")
+	c := openai.NewClient(OPENAI_API_KEY)
+	ctx := context.Background()
+	req := openai.ChatCompletionRequest{
+		Model:     openai.GPT3Dot5Turbo,
+		MaxTokens: 500,
+		Messages: []openai.ChatCompletionMessage{
+			{
+				Role:    openai.ChatMessageRoleUser,
+				Content: openaiPrompt,
+			},
+		},
+		Stream: true,
+	}
+	stream, err := c.CreateChatCompletionStream(ctx, req)
+	if err != nil {
+		log.Printf("ChatCompletionStream error: %v\n", err)
+		return
+	}
+	defer stream.Close()
+	
+	for {
+		response, err := stream.Recv()	
+		if errors.Is(err, io.EOF) {
+			break
+		}
+
+		if err != nil {
+			log.Printf("\nStream error: %v\n", err)
+			return
+		}
+		var responseContent = response.Choices[0].Delta.Content
+		writer.WriteString(responseContent)
+		writer.Flush()
+	}
+	fmt.Println(slack_buf.String())
+	sendSlackmessage(&slack_buf)
+}
+
+func sendSlackmessage(slack_buf *bytes.Buffer) {
+	fmt.Println(slack_buf.String())
+	var slackMessage = slack_buf.String()
+	formattedSlackMessage := fmt.Sprintf(strings.Replace(slackMessage, "\"", "", 3))
+	SLACK_API_KEY := os.Getenv("SLACK_API_KEY")
+	api := slack.New(SLACK_API_KEY)
+	CHANNEL_ID := os.Getenv("CHANNEL_ID")
+	channelID, timestamp, err := api.PostMessage(
+		CHANNEL_ID,
+		slack.MsgOptionText(formattedSlackMessage, false),
+		slack.MsgOptionAsUser(false), // Add this if you want that the bot would post message as a user, otherwise it will send response using the default slackbot
+	)
+	if err != nil {
+		log.Printf("%s", err)
+		return
+	}
+	log.Printf("Ingredients mixed and Slack message successfully sent to channel %s at %s", channelID, timestamp)
 }
